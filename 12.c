b929bdbd9d0bb6edfd864a7c960a186380e07360
Hakerei <hakerei@localhost.localdomain>
2021-12-12 05:14:46 +0300

    Small updates: nameless ID's

--- 12.c
+++ 12.c
@@ -5,32 +5,55 @@
 #include <sys/types.h>
 #include <pwd.h>
 #include <grp.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 int main(void)
 {
 
     // credentials - process identifiers
-    printf ("Process ID: %d, Parent Pricess ID: %d, Session ID: %d\n", getpid(), getppid(), getsid(0));
+    printf ("Process ID: %d, Parent Process ID: %d, Process Group ID: %d, Real GID: %d, Session ID: %d\n", getpid(), getppid(), getpgrp(), getegid(), getsid(0));
 
     struct passwd *pswd = getpwuid(getuid());
+    errno = 0;
     if (pswd == NULL)
     {
-        perror("Error in reading /etc/passwd");
-        return 1;
+        if (errno != 0)
+        {
+            perror("Error in reading /etc/passwd");
+            return 1;
+        }
+        else
+        {
+            printf("User ID: %d, User Name: ???\n", getuid());
+        }
     }
-    printf("User ID: %d, User Name: %s\n", getuid(), pswd->pw_name);
+    else
+    {
+        printf("User ID: %d, User Name: %s\n", getuid(), pswd->pw_name);
+    }
 
     struct group *grp = getgrgid(pswd->pw_gid);
+    errno = 0;
     if (grp == NULL)
     {
-        perror("Error in reading /etc/group");
-        return 2;
+        if (errno != 0)
+        {
+            perror("Error in reading /etc/group");
+            return 2;
+        }
+        else
+        {
+            printf("Group ID: %d, Group Name: ???\n", getgid());
+        }
     }
-    printf("Group ID: %d, Group Name: %s\n", getgid(), grp->gr_name);
-
+    else
+    {
+       printf("Group ID: %d, Group Name: %s\n", getgid(), grp->gr_name);
+    }
 
     long ngroups_max = sysconf(_SC_NGROUPS_MAX);
-    gid_t *groups = (gid_t *)malloc(ngroups_max *sizeof(gid_t));
+    gid_t *groups = (gid_t *)calloc(ngroups_max, sizeof(gid_t));
     int ngroups = getgroups(ngroups_max, groups);
     if (ngroups == -1)
     {
@@ -47,6 +70,6 @@ int main(void)
         printf("(%s); ", supgrp->gr_name);
     }
     printf("\n");
-
+    free(groups);
     return 0;
 }
